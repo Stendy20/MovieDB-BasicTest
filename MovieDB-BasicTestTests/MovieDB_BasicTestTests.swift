4821c718d75d4177bb579672092a2468e11b5d0b
Stendy Antonio <stendy20.sa@gmail.com>
2020-12-23 13:49:59 +0700

    Initial Commit

---
+++ MovieDB-BasicTestTests/MovieDB_BasicTestTests.swift
@@ -0,0 +1,33 @@
+//
+//  MovieDB_BasicTestTests.swift
+//  MovieDB-BasicTestTests
+//
+//  Created by Stendy Antonio on 23/12/20.
+//
+
+import XCTest
+@testable import MovieDB_BasicTest
+
+class MovieDB_BasicTestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
